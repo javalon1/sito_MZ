86bc9cd24cb928e6439210dfa7b6728b36b9664c
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-18 20:50:42 +0100

    cleaner code

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,29 +2,28 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Erastotenes.sito (30);
-        int k=2;
-        while (k<30) {
-            int licznik =0;
+public class App {
+    public static void main(String[] args) {
+        Erastotenes.sito(30);
+        int k = 2;
+        while (k < 30) {
+            int licznik = 0;
 
-            for (int i =2;i<k;i++){
-                if (k% i==0) {
+            for (int i = 2; i < k; i++) {
+                if (k % i == 0) {
                     licznik++;
                 }
 
             }
-            if (licznik ==0) {
-                System.out.println(k + " jest liczba pierwsza");
-            }else {
+            if (licznik == 0) {
+                System.out.println(k + " jest liczba pierwsza.");
+            } else {
+                System.out.println(k + " nie jest liczba pierwsza.");
             }
+
             k++;
-    }
+        }
     }
 }
 
--- src/main/java/com/sda/algorytmy/Erastotenes.java
+++ src/main/java/com/sda/algorytmy/Erastotenes.java
@@ -1,30 +1,30 @@
 package com.sda.algorytmy;
 
 public class Erastotenes {
-    public static int [] sito (int n) {
+    public static int[] sito(int n) {
 
-        int [] T = new  int [n+1];
+        int[] T = new int[n + 1];
 
         int i = 2;
         int w = 0;
 
-        while (i>n);
+        while (i > n) ;
 
-        while (!(i>=n)) {
-            w = i+i;
+        while (!(i >= n)) {
+            w = i + i;
             System.out.println(w);
-            while (!(w>n)) {
-                T[w] =1;
-                w=w +i;
+            while (!(w > n)) {
+                T[w] = 1;
+                w = w + i;
             }
-            i= i + 1;
+            i = i + 1;
 
         }
 
-        int [] liczbyPierwsze = new int [50];
-        for (int j =2; j<n;j++) {
+        int[] liczbyPierwsze = new int[50];
+        for (int j = 2; j < n; j++) {
 
-            if (T[j] ==0){
+            if (T[j] == 0) {
                 System.out.println(j + " ");
             }
         }
