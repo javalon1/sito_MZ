739a3b161f22968706a6683bdd90350ddd57c00e
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-18 12:52:36 +0100

    pierwsze zmiany w kodzie

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -8,6 +8,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int k=2;
+        while (k<200) {
+            int licznik =0;
+
+            for (int i =2;i<k;i++){
+                if (k% i==0) {
+                    licznik++;
+                }
+
+            }
+            if (licznik ==0) {
+                System.out.println(k + " jest liczba pierwsza");
+            }else {
+            }
+            k++;
+    }
     }
 }
